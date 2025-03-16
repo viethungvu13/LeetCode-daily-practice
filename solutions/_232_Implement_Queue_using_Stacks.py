7595e8fd648c66762a3df1038127e006764145a1
viethungvu13 <22022585@vnu.edu.vn>
2025-03-16 21:25:37 +0700

    update

---
+++ solutions/_232_Implement_Queue_using_Stacks.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+class MyQueue:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.appendleft(x)
+
+    def pop(self) -> int:
+        return self.queue.pop()
+
+    def peek(self) -> int:
+        return self.queue[-1]
+
+    def empty(self) -> bool:
+        if self.queue:
+            return False
+        return True
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
